c882661eba5448b32e45145b6d3fc6a5e862376c
Imani <lathomas64@gmail.com>
2024-08-25 17:18:10 -0400

    remove memory tracing from main

---
+++ prototype/hud.py
@@ -0,0 +1,4 @@
+from ursina import *
+
+class Hud(Entity):
+    pass
--- prototype/main.py
+++ prototype/main.py
@@ -1,10 +1,8 @@
 from ursina import *
 from grid import Map
+from hud import Hud
 from character import Character, AICharacter
 from ursina.prefabs.splash_screen import SplashScreen
-import linecache
-import os
-import tracemalloc
 from actions.actor import Actor
 #trying to put everything sofar together
 
@@ -18,39 +16,9 @@ def make_dummy(x,y):
     hex = Map.get_map()[x,y]
     print(hex.children)
 
-def display_top(snapshot, key_type='lineno', limit=3):
-    snapshot = snapshot.filter_traces((
-        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
-        tracemalloc.Filter(False, "<unknown>"),
-    ))
-    top_stats = snapshot.statistics(key_type)
-
-    print("Top %s lines" % limit)
-    for index, stat in enumerate(top_stats[:limit], 1):
-        frame = stat.traceback[0]
-        # replace "/path/to/module/file.py" with "module/file.py"
-        filename = os.sep.join(frame.filename.split(os.sep)[-2:])
-        print("#%s: %s:%s: %.1f KiB"
-              % (index, filename, frame.lineno, stat.size / 1024))
-        line = linecache.getline(frame.filename, frame.lineno).strip()
-        if line:
-            print('    %s' % line)
-
-    other = top_stats[limit:]
-    if other:
-        size = sum(stat.size for stat in other)
-        print("%s other: %.1f KiB" % (len(other), size / 1024))
-    total = sum(stat.size for stat in top_stats)
-    print("Total allocated size: %.1f KiB" % (total / 1024))
-
-def input(key):
-    if key == "p":
-        snapshot = tracemalloc.take_snapshot()
-        display_top(snapshot)
-        
 if __name__ == "__main__":
-    tracemalloc.start()
     app = Ursina()
+    ui = Hud()
     map = Map.create_map(5)
     player = Character(name="player")
     player.parent = map[(0,0)]
