a37cdabf504e24e7d905307e6529368a7e2d297e
Imani <lathomas64@gmail.com>
2024-09-11 14:36:19 -0400

    allow build script to overwrite and call bat file Panic at the portal

--- prototype/build.bat
+++ prototype/build.bat
@@ -1,3 +1,3 @@
-py -m ursina.build --include_modules=../email,../zipfile
+py -m ursina.build --overwrite --name "Panic at the Portal" --include_modules=../email,../zipfile
 cd build_Windows
 mklink prototype prototype.bat
