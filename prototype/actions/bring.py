6639e6a3d4afef92ba9d0abac6e942519b1a0bf9
Imani <lathomas64@gmail.com>
2024-09-12 12:41:46 -0400

    pylint bring it on

--- prototype/actions/bring.py
+++ prototype/actions/bring.py
@@ -1,22 +1,38 @@
+'''
+Bring it on action
+'''
+from sys import maxsize
+from ursina import color, Button, window, Func
 from prototype.actions.action import Action
 from prototype.hud import UI
 from prototype.fading_text import FadingText
-from ursina import color, Button, window, destroy, Func
-from sys import maxsize
+
 
 class BringAction(Action):
+    '''
+    Implementation of Bring it on action
+    5+ challenge any number of enemies within line of sight
+    basic action
+    '''
     def __init__(self, actor):
         super().__init__("5+",
                          "Bring it on!",
                          "Challenge any number of enemies within line of sight",
                          actor,
                          action_range=maxsize)
+        self.confirm = None
 
-    def confirm_targets(self, actor, die, targetHex):
-        if len(targetHex.children) == 0:
-            FadingText("No valid target", targetHex, color.red)
+    def confirm_targets(self, actor, die, target_hex):
+        '''
+        confirm_targets
+        because we have multiple targets we
+        add our target to a list and keep going
+        but setup a button to confirm all targets.
+        '''
+        if len(target_hex.children) == 0:
+            FadingText("No valid target", target_hex, color.red)
             return
-        target = targetHex.children[0]
+        target = target_hex.children[0]
         def challenge_targets():
             for targeted in UI.game_map.targeting["targets"]:
                 actor.challenge(targeted)
@@ -25,7 +41,8 @@ class BringAction(Action):
             self.confirm.disable()
 
         if not hasattr(self, "confirm"): # don't add button again if we already added it
-            self.confirm = Button("confirm targets", scale=(.3,.1), on_click=Func(challenge_targets))
+            self.confirm = Button("confirm targets", scale=(.3,.1),
+                                  on_click=Func(challenge_targets))
             self.confirm.x = window.top_right.x-.1
         else:
             print("reenabling confirm button")
