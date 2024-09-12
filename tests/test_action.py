8325389de27e061dc32d243dbf7c2618928e36c3
Imani <lathomas64@gmail.com>
2024-09-12 10:06:04 -0400

    remove stub tests

--- tests/test_action.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import sys, os 
-sys.path.append(os.path.abspath('..'))
-from ursina import *
-from prototype.actions.action import Action
-
-class TestAction(unittest.TestCase):
-    pass
--- tests/test_archetype.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import sys, os 
-sys.path.append(os.path.abspath('..'))
-from ursina import *
-from prototype.archetypes.archetype import Archetype
-
-class TestArchetype(unittest.TestCase):
-    pass
