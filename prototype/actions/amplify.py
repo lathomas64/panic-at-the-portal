b31082cfb94c9d1f3addb837d758450c14650be5
Imani <lathomas64@gmail.com>
2024-09-12 12:37:44 -0400

    pylint amplify

--- prototype/actions/amplify.py
+++ prototype/actions/amplify.py
@@ -1,4 +1,14 @@
-from prototype.actions.action import Action 
+'''
+module defining Amplify Action
+'''
+from prototype.actions.action import Action
 
 class AmplifyAction(Action):
-    pass
+    '''
+    Amplify action
+    3+ Amplify
+    Your next Action this turn has its range increased by 2 and can apply to 3 extra targets.
+    '''
+
+    def confirm_targets(self, actor, die, _target_hex):
+        raise NotImplementedError("next action +2 range +3 targets")
